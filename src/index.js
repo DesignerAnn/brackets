3e1c9356d715b8f27d72171d6065b0133c08b616
Hanna Kuksa <anya.kuksa.2001@gmail.com>
2023-02-02 12:25:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const openBrackets = bracketsConfig.map (function (item, index, array){
+    for(let i = 0; i < array.length; i++){
+      return item [i][0];
+    }
+  });
+
+  let closeBrackets = {};
+  for(let i = 0; i < bracketsConfig.length; i++){
+      closeBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  let result = [];
+
+  for(let i = 0; i < str.length; i++){
+    let currentBracket = str[i];
+    
+      if(openBrackets.includes(currentBracket)){
+        result.push(currentBracket);
+      } else {
+        if(result.length === 0){
+          return false;
+        } else {
+          if(closeBrackets[currentBracket] === result[result.length - 1]){
+            result.pop();
+          } else {
+            return false;
+          }
+        }
+      }
+      
+      for(let key in closeBrackets){
+        if((result[result.length - 1] === result[result.length - 2]) && (result[result.length - 1] === key)){
+          result.pop();
+          result.pop();
+        }
+      }
+  }
+
+  if(result.length === 0){
+    return true;
+  } else {
+    return false;
+  }
 }
